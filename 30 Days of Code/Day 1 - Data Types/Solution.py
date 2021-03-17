afa8e253546f2df369a41e4d4843acb7dbf413d0
Salma <salmasaa02@gmail.com>
2021-03-17 11:53:35 +0530

    Update Solution.py

--- 30 Days of Code/Day 1 - Data Types/Solution.py
+++ 30 Days of Code/Day 1 - Data Types/Solution.py
@@ -1,6 +1,5 @@
-Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = int (input())
+
+a = int (input())
 b = float(input())
 c = input()
 print (i + a)
