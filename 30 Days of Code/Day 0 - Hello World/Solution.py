96d38bf6144d614d20200b7f35a5beaf277da007
Salma <salmasaa02@gmail.com>
2021-03-17 11:50:11 +0530

    Update Solution.py

--- 30 Days of Code/Day 0 - Hello World/Solution.py
+++ 30 Days of Code/Day 0 - Hello World/Solution.py
@@ -1,6 +1,4 @@
-Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+
 input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
