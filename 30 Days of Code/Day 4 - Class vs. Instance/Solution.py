7b459b786eaafe23554a5cf6abffef5137560abd
Salma <salmasaa02@gmail.com>
2021-03-17 11:54:03 +0530

    Update Solution.py

--- 30 Days of Code/Day 4 - Class vs. Instance/Solution.py
+++ 30 Days of Code/Day 4 - Class vs. Instance/Solution.py
@@ -1,6 +1,5 @@
-Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Person:
+
+class Person:
     def __init__(self,initialAge):
         if initialAge > 0:
             self.age = initialAge
