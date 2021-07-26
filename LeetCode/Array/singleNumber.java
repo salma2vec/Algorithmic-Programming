5d0ed598e4aff20935a34e9101e4d7a80d368782
Salma <salmasaa02@gmail.com>
2021-07-26 22:30:57 +0530

    Create singleNumber.java

---
+++ LeetCode/Array/singleNumber.java
@@ -0,0 +1,14 @@
+// Single Number
+
+// Q) Given a non-empty array of integers nums, every element appears twice except for one. Find that single one. 
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
